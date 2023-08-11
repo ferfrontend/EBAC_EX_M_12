052fba66f7981d22464f40bc261f5a242cfdf881
Fernando Morais Cartaxo <fmoraiscartaxo@gmail.com>
2023-08-10 22:18:39 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('#telefone').mask('(00) 00000-0000');
+
+  $('form').validate({
+    rules: {
+      nome: {
+        required: true
+      },
+      email: {
+        required: true
+      },
+      telefone: {
+        required: true
+      }
+    }
+  })
+
+});
+
